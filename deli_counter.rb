dbd389cb68b00841b4b60e85a43ba5ad55f8891b
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-01-18 14:42:41 -0500

    commit tree 938728f536f40c1eed41b3228b3f5c8da40ba472

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require "pry"
+
+katz_deli = []
+
+def line(deli)
+  if deli.size > 0
+    deli_string = deli.each_with_index.map do |name, index|
+      "#{index + 1}. #{name}"
+    end.join(" ")
+    puts "The line is currently: #{deli_string}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(deli, name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.size} in line."
+end
+
+def now_serving(deli)
+  if deli.size > 0
+    puts "Currently serving #{deli.first}."
+    deli.shift
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
